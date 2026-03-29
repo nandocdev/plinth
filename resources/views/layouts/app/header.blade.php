7f793975fcdcf56859ad839a9fd27b276d835352
Fernando Castillo <fdocst@gmail.com>
2026-03-29 11:24:15 -0500

    feat(admin-rbac): agregar navegacion central para Admin Roles (visible solo con rol)

--- resources/views/layouts/app/header.blade.php
+++ resources/views/layouts/app/header.blade.php
@@ -28,6 +28,12 @@
                 :current="request()->routeIs('central.affiliates.*')" wire:navigate>
                 {{ __('Affiliates') }}
             </flux:navbar.item>
+            @can('admin-roles.viewAny')
+            <flux:navbar.item icon="shield-check" :href="route('central.admins.roles.index')"
+                :current="request()->routeIs('central.admins.roles.*')" wire:navigate>
+                {{ __('Admin Roles') }}
+            </flux:navbar.item>
+            @endcan
         </flux:navbar>
 
         <flux:spacer />
@@ -78,6 +84,12 @@
                     :current="request()->routeIs('central.affiliates.*')" wire:navigate>
                     {{ __('Affiliates') }}
                 </flux:sidebar.item>
+                @can('admin-roles.viewAny')
+                <flux:sidebar.item icon="shield-check" :href="route('central.admins.roles.index')"
+                    :current="request()->routeIs('central.admins.roles.*')" wire:navigate>
+                    {{ __('Admin Roles') }}
+                </flux:sidebar.item>
+                @endcan
             </flux:sidebar.group>
         </flux:sidebar.nav>
 
